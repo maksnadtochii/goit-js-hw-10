471287cad5e0e115e37b49eca39dbe1ce19e47ae
maksnadtochii <161505180+maksnadtochii@users.noreply.github.com>
2024-07-24 17:31:11 +0000

    Deploying to gh-pages from @ maksnadtochii/goit-js-hw-10@f0411ff7a503d5d174d8fe2198b0619c685d7c87 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{i as o}from"./assets/vendor-77e16229.js";document.querySelector(".form").addEventListener("submit",function(e){e.preventDefault();const t=e.target,s=Number(t.delay.value),r=t.state.value;m(s,r).then(i=>{o.success({title:"Success",message:`✅ Fulfilled promise in ${i}ms`})}).catch(i=>{o.error({title:"Error",message:`❌ Rejected promise in ${i}ms`})})});function m(e,t){return new Promise((s,r)=>{setTimeout(()=>{t==="fulfilled"?s(e):r(e)},e)})}
+//# sourceMappingURL=commonHelpers2.js.map
